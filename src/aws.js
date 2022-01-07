0ac304e8ad2767fa025cde64b4389b12c242d8f1
Francesco Pira <dev@fpira.com>
2022-01-07 19:46:45 +0100

    fixes

--- src/aws.js
+++ src/aws.js
@@ -1,5 +1,6 @@
 const html2text = require('html-to-text');
 const chromium = require('chrome-aws-lambda');
+const fx = require('./functions.js');
 
 async function notionToText(url){
   const browser = await chromium.puppeteer.launch({
@@ -15,8 +16,19 @@ async function notionToText(url){
   await page.goto(url, { waitUntil: 'networkidle0', timeout: 15000 });
 
   const title = await page.evaluate(() => document.title)
+
+  await page.waitForSelector('.notion-page-content', { timeout: 1000 });
+
+  const pageContent = await page.evaluate(() => {
+    return document.querySelector('.notion-page-content').innerHTML;
+  });
+
+  // covert downloaded html to text
   const text = html2text.convert(pageContent, {
     wordwrap: 80,
+    formatters: {
+      formatLinks: fx.formatAnchor,
+    },
     selectors: [
       { selector: 'img', format: 'skip' },
       { selector: 'a', format: 'formatLinks', options: { hideLinkHrefIfSameAsText: true, noAnchorUrl: true } }
@@ -37,7 +49,13 @@ exports.handler =  function(event, context, callback) {
 
   notionToText(url)
     .then(r => {
-      callback(null, r.text)
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          "content-type": "text/html"
+        },
+        body: r.text
+      })
     })
     .catch(e => callback(Error(e)))
 
